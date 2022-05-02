c7daaf579639d68ec3bf55c5535f068a6037419e
byRandom <tomasperezv11@gmail.com>
2022-05-02 20:46:36 +0200

    Vue 3 app

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<script>
+export default {
+  data: () => ({
+    contador: 10,
+  }),
+  methods: {
+    incrementar() {
+      this.contador++;
+    },
+    decrementar() {
+      this.contador--;
+    },
+    resetear() {
+      this.contador = 10;
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="container">
+    <img src="./assets/logo.svg" alt="logo vue" width="125" height="125" />
+    <small>
+      <h1>Hola Mundo Vue.js 3</h1>
+    </small>
+
+    <hr />
+
+    <h2>Contador: {{ contador }}</h2>
+    <button @click="incrementar()">+1</button>
+    <button @click="decrementar()">-1</button>
+    <button @click="resetear()">reset</button>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  text-align: center;
+}
+small {
+  color: #666;
+}
+h1 {
+  font-weight: bolder;
+}
+h2 {
+  font-size: 2.5rem;
+}
+
+button {
+  cursor: pointer;
+  font-size: 2rem;
+  border: 1px solid #666;
+  border-radius: 20px;
+  margin: 0px 6px;
+}
+</style>
